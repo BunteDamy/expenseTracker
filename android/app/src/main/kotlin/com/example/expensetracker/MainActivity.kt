57f69a0f0959cd76c24c1c8dd1baf1d7aec0a1f2
Damla Nur ALACAPUNAR <dlm_alaca@hotmail.com>
2025-04-02 20:18:29 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/expensetracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.expensesTracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/add_expense/blocs/get_categoriesbloc/get_categories_bloc_event.dart
@@ -0,0 +1,10 @@
+part of 'get_categories_bloc_bloc.dart';
+
+sealed class GetCategoriesBlocEvent extends Equatable {
+  const GetCategoriesBlocEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetCategories extends GetCategoriesBlocEvent {}
